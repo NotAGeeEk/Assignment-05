1a230e16d45e80a767663932ca3b60900724ad64
Amans <notshashank10@gmail.com>
2023-07-13 15:12:00 +0530

    first commit

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int attempts = 3;
+        string correctUsernameAdmin = "admin";
+        string correctPasswordAdmin = "passworD";
+        string correctUsernameJohn = "john";
+        string correctPasswordJohn = "pass123";
+
+        while (attempts > 0)
+        {
+            Console.Write("Enter username: ");
+            string username = Console.ReadLine();
+
+            Console.Write("Enter password: ");
+            string password = Console.ReadLine();
+
+            if (username == correctUsernameAdmin && password == correctPasswordAdmin)
+            {
+                Console.WriteLine("Login successful.");
+                break;
+            }
+            else if (username == correctUsernameJohn && password == correctPasswordJohn)
+            {
+                Console.WriteLine("Login successful.");
+                break;
+            }
+            else if (username == correctUsernameAdmin || username == correctUsernameJohn)
+            {
+                Console.WriteLine("Invalid password.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid username.");
+            }
+
+            attempts--;
+
+            if (attempts == 0)
+            {
+                Console.WriteLine("Login failed.");
+            }
+        }
+
+        Console.ReadLine();
+    }
+}
